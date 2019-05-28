c388e8833255456a2ae5940919b3ea24008e4c22
mishale22 <sami_na11@hotmail.com>
2019-05-28 15:00:03 -0500

    remove sendgrid credential

--- colart-meteor/server/main.js
+++ colart-meteor/server/main.js
@@ -3,5 +3,4 @@ import '../imports/api/artists.js';
 import '../imports/api/email.js';
 Meteor.startup(() => {
   // code to run on server at startup
-  process.env.MAIL_URL = 'smtp://mishale22:SANDRAmishale0613@smtp.sendgrid.net:587?tls.rejectUnauthorized=false';
 });
